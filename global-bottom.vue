ed5383dbda4b1156c55566542ff602176084dc4d
Nile <nilethebot@users.noreply.github.com>
2026-02-26 13:45:18 +0000

    Add subtle footer (@rida · rida.me) on all slides except title/closing

---
+++ global-bottom.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    v-if="$slidev.nav.currentPage > 1 && $slidev.nav.currentPage < $slidev.nav.total"
+    class="absolute bottom-4 right-6 text-[11px] text-white/20 font-light tracking-wide z-50"
+  >
+    @rida · rida.me
+  </div>
+</template>
